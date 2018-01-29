4ed11561ef322cc76ec68c355840ef75ec1954f8
Hugo Queiros <hugofqueiros@gmail.com>
2018-01-29 23:07:39 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'babel-polyfill';
+
 import App from './App.vue'
+import router from './router';
+
+Vue.use(BootstrapVue);
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
